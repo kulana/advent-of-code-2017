f3a78b4eee904e1462356fa010e448c2d3ca70fd
JW Janse <jaco.janse@trivident.com>
2017-12-10 08:31:37 +0100

    Solution for the Knot Hash

---
+++ December10/KnotHash/Algorithm.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace KnotHash
+{
+    public class Algorithm<T>
+    {
+        public void Apply(IList<T> list, IEnumerable<int> lengths, int startPosition)
+        {
+            int listSize = list.Count;
+            var skipSize = 0;
+            var currentPosition = startPosition;
+            foreach (var length in lengths)
+            {
+                ReverseCircular(list, currentPosition, length);
+                currentPosition = (currentPosition + length + skipSize) % listSize;
+                skipSize++;
+            }
+        }
+
+        /// <summary>
+        /// reverses num items in the list, starting from startPos
+        /// if the length extends beyond the length of the list, it wraps around to the front
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        private void ReverseCircular(IList<T> list, int index, int count)
+        {
+            int listSize = list.Count;
+            if (count > listSize)
+            {
+                return;
+            }
+
+            // determine values in sublist
+            List<T> subList = new List<T>(count);
+            for (int i = 0; i < count; i++)
+            {
+                int actualIndex = (index + i) % listSize;
+                subList.Add(list[actualIndex]);
+            }
+            // reverse sublist
+            subList.Reverse();
+            // place sublist items back in original list
+            for (int i = 0; i < count; i++)
+            {
+                int actualIndex = (index + i) % listSize;
+                list[actualIndex] = subList[i];
+            }
+        }
+    }
+}
---
+++ December10/KnotHash/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KnotHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Enumerable.Range(0, 256).ToList();
+            var algorithm = new Algorithm<int>();
+            var lengths = new List<int>() { 197, 97, 204, 108, 1, 29, 5, 71, 0, 50, 2, 255, 248, 78, 254, 63 };
+            algorithm.Apply(list, lengths, 0);
+            Console.WriteLine($"The answer is {list[0] * list[1]}");
+        }
+    }
+}
