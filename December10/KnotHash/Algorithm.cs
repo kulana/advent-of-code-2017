c7b944b5042014bfe7f608542cd7bf6d93b0ff7a
JW Janse <jaco.janse@trivident.com>
2017-12-10 10:34:18 +0100

    Moved circular reverse into extension method

--- December10/KnotHash/Algorithm.cs
+++ December10/KnotHash/Algorithm.cs
@@ -1,53 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using KnotHash.Extensions;
+using System.Collections.Generic;
 
 namespace KnotHash
 {
-    public class Algorithm<T>
+    public class Algorithm
     {
-        public void Apply(IList<T> list, IEnumerable<int> lengths, int startPosition)
+        public void Apply<T>(IList<T> list, IEnumerable<int> lengths, int startPosition)
         {
             int listSize = list.Count;
             var skipSize = 0;
             var currentPosition = startPosition;
             foreach (var length in lengths)
             {
-                ReverseCircular(list, currentPosition, length);
+                list.ReverseCircular(currentPosition, length);
                 currentPosition = (currentPosition + length + skipSize) % listSize;
                 skipSize++;
-            }
-        }
-
-        /// <summary>
-        /// reverses num items in the list, starting from startPos
-        /// if the length extends beyond the length of the list, it wraps around to the front
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
-        /// <param name="startIndex"></param>
-        /// <param name="length"></param>
-        /// <returns></returns>
-        private void ReverseCircular(IList<T> list, int index, int count)
-        {
-            int listSize = list.Count;
-            if (count > listSize)
-            {
-                return;
-            }
-
-            // determine values in sublist
-            List<T> subList = new List<T>(count);
-            for (int i = 0; i < count; i++)
-            {
-                int actualIndex = (index + i) % listSize;
-                subList.Add(list[actualIndex]);
-            }
-            // reverse sublist
-            subList.Reverse();
-            // place sublist items back in original list
-            for (int i = 0; i < count; i++)
-            {
-                int actualIndex = (index + i) % listSize;
-                list[actualIndex] = subList[i];
             }
         }
     }
---
+++ December10/KnotHash/Extensions/ListExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace KnotHash.Extensions
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// reverses num items in the list, starting from startPos
+        /// if the length extends beyond the length of the list, it wraps around to the front
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static IList<T> ReverseCircular<T>(this IList<T> list, int index, int count)
+        {
+            int listSize = list.Count;
+            if (count > listSize)
+            {
+                return list;
+            }
+
+            // determine values in sublist
+            List<T> subList = new List<T>(count);
+            for (int i = 0; i < count; i++)
+            {
+                int actualIndex = (index + i) % listSize;
+                subList.Add(list[actualIndex]);
+            }
+            // reverse sublist
+            subList.Reverse();
+            // place sublist items back in original list
+            for (int i = 0; i < count; i++)
+            {
+                int actualIndex = (index + i) % listSize;
+                list[actualIndex] = subList[i];
+            }
+            return list;
+        }
+    }
+}
