c8e1ac116082f103885725b2f4a5f9968a2600d4
JW Janse <jaco.janse@trivident.com>
2017-12-16 20:33:04 +0100

    Added solution for puzzle 2, remembering result of each dance and checking if we have seen it before. If we do after X iterations, then we know how many following iterations we still have to do: 1 billion % X

--- December16/PermutationPromenade/Program.cs
+++ December16/PermutationPromenade/Program.cs
@@ -15,16 +15,35 @@ namespace PermutationPromenade
             var fileProcessor = new FileProcessor();
             string input = fileProcessor.ReadFileToEnd(filePath);
             var moves = new List<DanceMove>();
+            var memory = new List<string>();
 
             // treat programs as a list of numbers, those are easier to replace
             var bytes = Encoding.ASCII.GetBytes("abcdefghijklmnop").ToList();
             foreach (var value in input.Split(','))
             {
-                var move = Parser.Parse(value);
-                move.Perform(bytes);
+                moves.Add(Parser.Parse(value));
             }
- 
-            Console.WriteLine($"Final result = {Encoding.UTF8.GetString(bytes.ToArray())}");
+
+            bytes = Encoding.ASCII.GetBytes("abcdefghijklmnop").ToList();
+            var numIterations = 1000000000;
+            int i = 1;
+            while (i <= numIterations)
+            {
+                moves.ForEach(move => move.Perform(bytes));
+                Console.WriteLine($"Result for iteration {i} = {Encoding.UTF8.GetString(bytes.ToArray())}");
+                string result = Encoding.UTF8.GetString(bytes.ToArray());
+                if (memory.Contains(result))
+                {
+                    Console.WriteLine($"Same string after {i} iterations");
+                    var resultIndex = 1000000000 % (i-1);
+                    Console.WriteLine($"Final result = {memory[resultIndex-1]}");
+                    break;
+                }
+                else {
+                    memory.Add(Encoding.UTF8.GetString(bytes.ToArray()));
+                    i++;
+                }
+            }
         }
     }
 }
