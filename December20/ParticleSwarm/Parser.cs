9ace37a1d4b1c380b14f1fa8c776ca5af864a76d
JW Janse <jaco.janse@trivident.com>
2017-12-20 16:25:45 +0100

    Fixed parser to use regexp for parsing the line into particle properties

--- December20/ParticleSwarm/Parser.cs
+++ December20/ParticleSwarm/Parser.cs
@@ -1,28 +1,26 @@
-﻿namespace ParticleSwarm
+﻿using System.Text.RegularExpressions;
+
+namespace ParticleSwarm
 {
     public class Parser
     {
+        private const string Pattern = @"\w=<(?<x>-?\d+),(?<y>-?\d+),(?<z>-?\d+)>";
+
+        // paser p=<-717,-4557,2578>, v=<153,21,30>, a=<-8,8,-7>
         public Particle Parse(string line)
         {
-            var parts = line.Split(',');
-            var position = GetSpecs(parts[0]);
-            var velocity = GetSpecs(parts[1]);
-            var acceleration = GetSpecs(parts[2]);
-            return new Particle(position, velocity, acceleration);
+            MatchCollection matches = Regex.Matches(line, Pattern, RegexOptions.IgnorePatternWhitespace);
+            return new Particle(GetSpecs(matches[0].Groups), GetSpecs(matches[1].Groups), GetSpecs(matches[2].Groups));
         }
 
         // can parse a value like p=<-717,-4557,2578>
-        private Specs3D GetSpecs(string input)
+        private Specs3D GetSpecs(GroupCollection values)
         {
-            var parts = input.Split('=');
-            // remove first and last characters
-            var rawValues = parts[1].TrimStart('<').TrimEnd('>');
-            var values = rawValues.Split(',');
             return new Specs3D
             {
-                X = int.Parse(values[0]),
-                Y = int.Parse(values[1]),
-                Z = int.Parse(values[2])
+                X = int.Parse(values["x"].Value),
+                Y = int.Parse(values["y"].Value),
+                Z = int.Parse(values["z"].Value)
             };
         }
     }
